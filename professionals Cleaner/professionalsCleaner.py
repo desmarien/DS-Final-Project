05efc9573c945419decf8d66193c12a2f1ad528a
Sebastian Cortes <jscortes@smcm.edu>
2019-04-04 15:27:59 -0400

    Upload file
    professionalsCleaner - Cleans the data within the professionals.csv file. It checks for whether an entry has a blank section for its professional_industry or professional_headline. If it doesn't, then it adds the row to a separate csv file named 'professionalsCLEAN.csv'

---
+++ professionals Cleaner/professionalsCleaner.py
@@ -0,0 +1,47 @@
+
+# coding: utf-8
+
+# In[12]:
+
+
+import numpy as np
+import pandas as pd
+import csv
+
+#load in the professionals csv
+data = pd.read_csv("professionals.csv")
+csvFile = open('professionalsCLEAN.csv','a')
+
+#opens writer
+csvWriter = csv.writer(csvFile)
+
+#creates new row with the needed column titles
+csvWriter.writerow(['professionals_id', 'professionals_location', 'professionals_industry', 'professionals_headline', 'professionals_date_joined'])
+
+#copy of data, replaces the NaN within the csv file with np.nan
+testData = data
+testData['professionals_industry'].replace('', np.nan, inplace=True)
+testData['professionals_headline'].replace('', np.nan, inplace=True)
+
+#iterates through the data, if either the industry or headlines cells are not empty, add the info to the csv file
+for i in range(len(data)):
+    industry = data.loc[i]['professionals_industry']
+    headline = data.loc[i]['professionals_headline']
+    if headline is not np.nan or industry is not np.nan:
+        newInfo = data.loc[i]
+        
+        #variables we're going to be cleaning
+        info = ['professionals_industry','professionals_headline','professionals_location']
+        
+        #removes the nan string 
+        for x in info:
+            if newInfo[x] is np.nan:
+                newInfo[x] = ''
+        
+        #writes as new row within the professionalsCLEAN.csv
+        csvWriter.writerow(newInfo)
+        
+#closes the file reader
+print("done")
+csvFile.close()
+
