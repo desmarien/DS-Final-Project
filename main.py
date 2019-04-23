9263f2ec02a8038002dbcce004b2b1ba9a2abe52
Harrison Ratcliffe <hhratcliffe@smcm.edu>
2019-04-23 10:44:58 -0400

    Gather pro questions
    - Gathers List of all questions answered by each pro and add to professionals dataframe as a column
    - Commented out line 39, which dropped the 'questions_body' column of questions dataframe
    - added some comments

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ import csv
 #DATA CLEANING
 
 #initialize dataframes
-professionals_df = pd.read_csv("Final Project Data/professionals.csv")
-questions_df = pd.read_csv("Final Project Data/questions.csv")
-answers_df = pd.read_csv("Final Project Data/answers.csv")
+professionals_df = pd.read_csv("D:/DS_CSVs/professionals.csv")
+questions_df = pd.read_csv("D:/DS_CSVs/questions.csv")
+answers_df = pd.read_csv("D:/DS_CSVs/answers.csv")
 
 # clean out questions.csv to only include: id, question title, question body
 questions_df = questions_df[['questions_id', 'questions_title', 'questions_body']]
@@ -31,12 +31,12 @@ def getKeywords(df, column_name, new_column_name):
         keywords_scores = r.get_word_degrees()
         # assigning the key words to the new column
         row[new_column_name] = list(keywords_scores.keys())
-#get keywords for title and question body        
+#get keywords for title and question body
 getKeywords(questions_df, 'questions_title', 'questions_keywords')
 getKeywords(questions_df, 'questions_body', 'questions_body_keywords')
 
 #get rid of the questions_body column, since we don't need it anymore
-questions_df.drop(columns = ['questions_body'], inplace = True)
+#questions_df.drop(columns = ['questions_body'], inplace = True) #commented this out as question_body is needed later
 
 # set the indexes of the dataframe as the question ids instead of integers, that way we can just pull the id if needed
 questions_df.set_index('questions_id', inplace = True)
@@ -51,11 +51,11 @@ for index, row in questions_df.iterrows():
         if col != 'questions_title':
             word_string = word_string + ' '.join(row[col])+ ' '
     row['KeyWords'] = word_string
-    
+
 #get rid of the original two keywords columns since we combined them
 questions_df.drop(columns = ['questions_keywords', 'questions_body_keywords'], inplace = True)
 #testing changes, create a csv of dataframe
-questions_df.to_csv('Final Project Data\questionsKeywords.csv')
+questions_df.to_csv('D:/DS_CSVs/questionsKeywords.csv')
 
 #drop everything from professionals df other than id, since they're not needed
 professionals_df.drop(columns = ['professionals_location', 'professionals_industry', 'professionals_date_joined',
@@ -79,20 +79,26 @@ for x in professionals_df['professionals_id']:
 #now that we have the shortened list of professionals, gather the question id's that were answered by each pro
 # within the answers df... then grab the question data corresponding to the ids
 professionals_df['answered_questions'] = ''
+
 for y in professionals_df['professionals_id']:
+    # y is a professionals id
     #find all indexes where the pro id occurs within the answers df
     location_list = answers_df.answers_author_id[answers_df.answers_author_id == y].index.tolist()
     #use those indexes to get question id's from answers df
-    question_list = []
+    question_list = [] # list of question id's answered by the professional
     for loc in location_list:
-        question_list.append(answers_df.answers_question_id[loc])
+        question_list.append(answers_df.answers_question_id[loc]) # construct list of question ids by prof
     #with the question id's, gather all the question keywords into a single word string and throw it into the new
     # column in professionals
-    all_keywords_string = ""
+    questions_text_list = list() # list of questions body text
     for q in question_list:
-        all_keywords_string = all_keywords_string + questions_df.KeyWords[q]
-        professionals_df.answered_questions[professionals_df.professionals_id == y] = all_keywords_string
-    #now that we have the keywords, we want to set pro id as index for later searches
+        # get question text from question_df using q (question_id) as an index
+        questions_text_list.append(questions_df.questions_body[q])  # append to list of questions answered by the professional
+
+    index = professionals_df.index[professionals_df['professionals_id'] == y][0] # get row numeric index based on the professionals id
+    professionals_df.at[index, 'answered_questions'] = questions_text_list # add the professionals list of questions to professionals dataframe
+
+#now that we have the keywords, we want to set pro id as index for later searches
 professionals_df.set_index('professionals_id', inplace = True)
 # convert professionals df to csv so we can read it for testing
-professionals_df.to_csv('Final Project Data\professionalsKeywords.csv')
+professionals_df.to_csv('D:/DS_CSVs/professionalsKeywords.csv')
